f9aec7469a17dc61cc3cdc796adf69e9c28c896d
leebunce <leebunce@hotmail.com>
2020-03-08 14:17:56 +0000

    Simplify validation

--- Validation/Validation.R
+++ Validation/Validation.R
@@ -10,22 +10,12 @@ load('Output/info_democracy.Rdata')
 # Rules -------------------------------------------------------------------
 validator_info_democracy <- validator(.file = 'Validation/Rules/info_democracy.yaml')
 
-# Error function ----------------------------------------------------------
-get_errors <- function(df, rules, key){
-  confront(df, rules, key = key) %>% 
-    as.data.frame() %>% 
-    left_join(as.data.frame(rules), by = 'name') %>% 
-    filter(value == F) %>% 
-    select(-value, -expression, -label) %>% 
-    #arrange(key, name) %>% Need tidyeval for this step I think 
-    rename(error = name)
-}
-
 # Validate ----------------------------------------------------------------
-errors_info_democracy <- get_errors(info_democracy, validator_info_democracy, key = "dntn_ec_ref")
-
-# Print errors ------------------------------------------------------------
-file.remove(list.files('Validation/Errors', full.names = T))
-if(nrow(errors_info_democracy) > 0) write_csv(errors_info_democracy, 'Validation/Errors/errors_info_democracy.csv')
+errors_info_democracy <- confront(info_democracy, validator_info_democracy, key = "dntn_ec_ref") %>% 
+  as.data.frame() %>% 
+  left_join(as.data.frame(validator_info_democracy), by = 'name') %>% 
+  filter(value == F) %>% 
+  select(-value, -expression, -label) %>% 
+  rename(error = name)
 
 print(paste('There are', nrow(errors_info_democracy), 'errors in the info_democracy file.'))
