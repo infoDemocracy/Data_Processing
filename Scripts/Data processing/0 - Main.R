3fa9184a1b32f23ba52a9a1fdd723770cad3d153
leebunce <leebunce@hotmail.com>
2020-03-09 20:39:42 +0000

    Run validation as part of data processing

--- Scripts/Data processing/0 - Main.R
+++ Scripts/Data processing/0 - Main.R
@@ -2,8 +2,12 @@
 
 # This script downloads the latest data from the Electoral Commission.
 # It then processes the data and saves an output file.
+# It then runs some validation checks on the output file.
 
 # Scripts -----------------------------------------------------------------
 source('Scripts/Data processing/1 - Get donations.R')
 source('Scripts/Data processing/2 - Get loans.R')
 source('Scripts/Data processing/3 - Produce output data.R')
+
+# Validation --------------------------------------------------------------
+source('Validation/Validation.R')
