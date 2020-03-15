784386603b91a1fcdde5c521852d961e9e141dbd
leebunce <leebunce@hotmail.com>
2020-03-15 13:32:52 +0000

    Use RinfoDemocracy

--- Scripts/Data processing/1 - Get donations.R
+++ Scripts/Data processing/1 - Get donations.R
@@ -6,91 +6,11 @@
 
 # Packages ----------------------------------------------------------------
 library(dplyr)
-library(tidyr)
-library(tibble)
 library(readr)
-library(httr)
-library(stringr)
-library(lubridate)
+library(RinfoDemocracy)
 
 # Get raw data from Electoral Commission ----------------------------------
-ec_donations_raw <- GET('http://search.electoralcommission.org.uk/api/csv/Donations',
-                   query = list(sort = "AcceptedDate",
-                                order = "desc",
-                                prePoll = "true",
-                                postPoll = "true")) %>% 
-  content(type = "text/csv",
-          encoding = 'UTF-8',
-          col_types = cols(
-            ECRef = col_character(),
-            RegulatedEntityName = col_character(),
-            RegulatedEntityType = col_character(),
-            Value = col_character(),
-            AcceptedDate = col_date(format = '%d/%m/%Y'),
-            AccountingUnitName = col_character(),
-            DonorName = col_character(),
-            AccountingUnitsAsCentralParty = col_logical(),
-            IsSponsorship = col_logical(),
-            DonorStatus = col_character(),
-            RegulatedDoneeType = col_character(),
-            CompanyRegistrationNumber = col_character(),
-            Postcode = col_character(),
-            DonationType = col_character(),
-            NatureOfDonation = col_character(),
-            PurposeOfVisit = col_character(),
-            DonationAction = col_character(),
-            ReceivedDate = col_date(format = '%d/%m/%Y'),
-            ReportedDate = col_date(format = '%d/%m/%Y'),
-            IsReportedPrePoll = col_logical(),
-            ReportingPeriodName = col_character(),
-            IsBequest = col_logical(),
-            IsAggregation = col_logical(),
-            RegulatedEntityId = col_double(),
-            AccountingUnitId = col_double(),
-            DonorId = col_double(),
-            CampaigningName = col_character(),
-            RegisterName = col_character(),
-            IsIrishSource = col_logical()
-          ),
-          na = c("", "NA", "N/A"))
-
-stop_for_problems(ec_donations_raw)
-
-# Process -----------------------------------------------------------------
-ec_donations_raw <- 
-  ec_donations_raw %>% 
-  rename(dntn_ec_ref = ECRef,
-         dntn_regulated_entity_name = RegulatedEntityName,
-         dntn_regulated_entity_type = RegulatedEntityType,
-         dntn_value = Value,
-         dntn_accepted_date = AcceptedDate,
-         dntn_accounting_unit_name = AccountingUnitName,
-         dntn_donor_name = DonorName,
-         dntn_accounting_unit_as_central_party = AccountingUnitsAsCentralParty,
-         dntn_is_sponsorship = IsSponsorship,
-         dntn_donor_status = DonorStatus,
-         dntn_regulated_donee_type = RegulatedDoneeType,
-         dntn_company_registration_number = CompanyRegistrationNumber,
-         dntn_postcode = Postcode,
-         dntn_donation_type = DonationType,
-         dntn_nature_of_donation = NatureOfDonation,
-         dntn_purpose_of_visit = PurposeOfVisit,
-         dntn_donation_action = DonationAction,
-         dntn_received_date = ReceivedDate,
-         dntn_reported_date = ReportedDate,
-         dntn_is_reported_pre_poll = IsReportedPrePoll,
-         dntn_reporting_period_name = ReportingPeriodName,
-         dntn_is_bequest = IsBequest,
-         dntn_is_aggregation = IsAggregation,
-         dntn_regulated_entity_id = RegulatedEntityId,
-         dntn_accounting_unit_id = AccountingUnitId,
-         dntn_donor_id = DonorId,
-         dntn_campaigning_name = CampaigningName,
-         dntn_register_name = RegisterName,
-         dntn_is_irish_source = IsIrishSource) %>%
-  mutate(dntn_value = as.numeric(str_replace_all(dntn_value, '[\\£|,]', '')),
-         download_date = today(),
-         type = "Donation")
+ec_donations_raw <- get_donations()
 
 # Update donation-donor link ----------------------------------------------
 
